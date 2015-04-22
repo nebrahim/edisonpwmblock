64da82c5f5de177568a5763a25ec256b7fc1a00d
nomeo <nebrahim@umich.edu>
2015-04-22 05:24:58 +0000

    Added keyboard control

--- test.py
+++ test.py
@@ -1,22 +1,52 @@
 from RC import RC
+from getch import getch
 import time
 
+speed = 0;
+
 grasshopper = RC(0, 1);
-
+grasshopper.setServoAngle(0);
+grasshopper.setSpeed(0);
+ 
 while (True):
-  grasshopper.setServoAngle(0);
-  grasshopper.setSpeed(0);
-  time.sleep(1); 
+  key = ord(getch());
+  print key;
 
-  grasshopper.setSpeed(500);
-  grasshopper.setServoAngle(50);
-  time.sleep(1);
+  if key == 3: # ctrl-C
+    grasshopper.setServoAngle(0);
+    grasshopper.setSpeed(0);
+    break;
 
-  grasshopper.setSpeed(-500);
-  grasshopper.setServoAngle(-50);
-  time.sleep(.1);
-  grasshopper.setSpeed(0);
-  time.sleep(.1);
-  grasshopper.setSpeed(-500);
-  time.sleep(1);
+  if key == 115: # s
+    grasshopper.setServoAngle(0);
+    grasshopper.setSpeed(0);
+    speed = 0;
+  
+  elif key == 27: # up/down/left/right
+    key = ord(getch());
+    
+    if key == 91: # up/down/left/right
+      key = ord(getch());
 
+      if key == 65: # up
+        if (speed < 1500):
+	  speed = speed + 500;
+        grasshopper.setSpeed(speed);
+
+      elif key == 66: # down
+        if (speed > -1500):
+	  speed = speed - 500;
+        grasshopper.setSpeed(speed);
+	time.sleep(.1);
+	grasshopper.setSpeed(0);
+	time.sleep(.1);
+	grasshopper.setSpeed(speed);
+
+      elif key == 67: # right
+        grasshopper.setServoAngle(50);
+
+      elif key == 68: # left
+        grasshopper.setServoAngle(-50);
+#
+#  elif key == 224: # Special Keys (arrows, functions keys, etc.)
+#    key = ord(getch());
